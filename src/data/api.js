95269392dccc10fdeb584e77e799ca2c7b48b07f
tad27 <tadesseakele27@gmail.com>
2023-04-07 12:34:14 -0700

    Fix error

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,6 @@
 const BASE_URL = "http://localhost:3000";
 export const getAllTasks = async () => {
-  const res = await fetch(`${BASE_URL}/tasks`, {"no-cache"});
+  const res = await fetch(`${BASE_URL}/tasks`);
   const tasks = await res.json();
   return tasks;
 };
